a25b28623732736c37d49540a7406b49c48edc85
badhitman <10000335+badhitman@users.noreply.github.com>
2020-07-07 10:19:52 +0300

    ~ fix

---
+++ server configurator/Services/logg/LogListAdapter.cs
@@ -0,0 +1,39 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman 
+////////////////////////////////////////////////
+
+using System.Linq;
+using ab.Model;
+using Android.Views;
+using AndroidX.RecyclerView.Widget;
+
+namespace ab.Services
+{
+    public class LogListAdapter : RecyclerView.Adapter
+    {
+        public override int ItemCount { get { using (LogsContext db = new LogsContext()) { return db.Logs.Count(); } } }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            LogViewHolder vh = holder as LogViewHolder;
+            lock (LogsContext.DbLocker)
+            {
+                using (LogsContext db = new LogsContext())
+                {
+                    LogRowModel row = db.Logs.OrderByDescending(x=>x.CreatedAt).Skip(position).FirstOrDefault();
+                    vh.LogMessageTextMessage.Text = row.Name;
+                    vh.LogMessageDateTimeStamp.Text = row.CreatedAt.ToString();
+                    vh.LogMessageStatus.Text = row.Status.ToString();
+                }
+            }
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.log_list_item, parent, false);
+
+            LogViewHolder vh = new LogViewHolder(itemView);
+            return vh;
+        }
+    }
+}
---
+++ server configurator/Services/logg/LogViewHolder.cs
@@ -0,0 +1,24 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman 
+////////////////////////////////////////////////
+
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+
+namespace ab.Services
+{
+    public class LogViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView LogMessageTextMessage { get; private set; }
+        public TextView LogMessageDateTimeStamp { get; private set; }
+        public TextView LogMessageStatus { get; private set; }
+
+        public LogViewHolder(View itemView) : base(itemView)
+        {
+            LogMessageTextMessage = itemView.FindViewById<TextView>(Resource.Id.log_list_item_message_text_message);
+            LogMessageDateTimeStamp = itemView.FindViewById<TextView>(Resource.Id.log_list_item_message_date_time_stamp);
+            LogMessageStatus = itemView.FindViewById<TextView>(Resource.Id.log_list_item_message_status);
+        }
+    }
+}
