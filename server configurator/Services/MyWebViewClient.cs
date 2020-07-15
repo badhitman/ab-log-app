54257b2ed279286a5dc0e5d8c8249c4e743cd16e
badhitman <10000335+badhitman@users.noreply.github.com>
2020-07-15 11:14:12 +0300

    ~ fix

--- server configurator/Services/MyWebViewClient.cs
+++ server configurator/Services/MyWebViewClient.cs
@@ -21,7 +21,7 @@ namespace ab.Services
             {
                 string basePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
                 string full_www_path = Path.Combine(basePath, "www");
-                if(!Directory.Exists(full_www_path))
+                if (!Directory.Exists(full_www_path))
                 {
                     Directory.CreateDirectory(full_www_path);
                 }
@@ -29,141 +29,41 @@ namespace ab.Services
             }
         }
 
-        public static string bootstrap_min_css
-        {
-            get
-            {
-                string file_name = "bootstrap.min.css";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string bootstrap_min_js
-        {
-            get
-            {
-                string file_name = "bootstrap.min.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string jquery_slim_min_js
-        {
-            get
-            {
-                string file_name = "jquery.slim.min.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string popper_min_js
-        {
-            get
-            {
-                string file_name = "popper.min.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_root_js
-        {
-            get
-            {
-                string file_name = "onload.root.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_pt_js
-        {
-            get
-            {
-                string file_name = "onload.pt.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_cf1_js
-        {
-            get
-            {
-                string file_name = "onload.cf1.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_cf2_js
-        {
-            get
-            {
-                string file_name = "onload.cf2.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_cf3_js
-        {
-            get
-            {
-                string file_name = "onload.cf3.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_cf4_js
-        {
-            get
-            {
-                string file_name = "onload.cf4.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_cf7_js
-        {
-            get
-            {
-                string file_name = "onload.cf7.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
-
-        public static string onload_cf9_js
-        {
-            get
-            {
-                string file_name = "onload.cf9.js";
-                string file_path = Path.Combine(WebPathBase, file_name);
-                return file_path;
-            }
-        }
+        public static string bootstrap_min_css => Path.Combine(WebPathBase, "bootstrap.min.css");
+        public static string bootstrap_min_js => Path.Combine(WebPathBase, "bootstrap.min.js");
+        public static string jquery_slim_min_js => Path.Combine(WebPathBase, "jquery.slim.min.js");
+        public static string popper_min_js => Path.Combine(WebPathBase, "popper.min.js");
+        public static string onload_root_js => Path.Combine(WebPathBase, "onload.root.js");
+        public static string onload_pt_js => Path.Combine(WebPathBase, "onload.pt.js");
+        public static string onload_cf1_js => Path.Combine(WebPathBase, "onload.cf1.js");
+        public static string onload_cf2_js => Path.Combine(WebPathBase, "onload.cf2.js");
+        public static string onload_cf3_js => Path.Combine(WebPathBase, "onload.cf3.js");
+        public static string onload_cf4_js => Path.Combine(WebPathBase, "onload.cf4.js");
+        public static string onload_cf7_js => Path.Combine(WebPathBase, "onload.cf7.js");
+        public static string onload_cf9_js => Path.Combine(WebPathBase, "onload.cf9.js");
 
         [System.Obsolete]
         public override bool ShouldOverrideUrlLoading(WebView view, string url)
         {
-            //return base.ShouldOverrideUrlLoading(view, url);
             Log.Debug(TAG, $"ShouldOverrideUrlLoading - {url}");
-            ShouldUrlLoading?.Invoke(url);
+            if(ShouldUrlLoading == null)
+            {
+                return base.ShouldOverrideUrlLoading(view, url);
+            }
+            //
+            ShouldUrlLoading.Invoke(url);
             return true;
         }
 
         public override bool ShouldOverrideUrlLoading(WebView view, IWebResourceRequest request)
         {
-            //return base.ShouldOverrideUrlLoading(view, request);
             string url = request.Url.ToString();
             Log.Debug(TAG, $"ShouldOverrideUrlLoading - {url}");
-            ShouldUrlLoading?.Invoke(request.Url.ToString());
+            if(ShouldUrlLoading == null)
+            {
+                return base.ShouldOverrideUrlLoading(view, request);
+            }
+            ShouldUrlLoading.Invoke(request.Url.ToString());
             return true;
         }
     }
